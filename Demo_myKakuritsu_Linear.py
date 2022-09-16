19671daa389662ef00058bc84e2c170b0a5d358a
UEFI-code <1092700291@qq.com>
2022-09-16 12:12:45 +0800

    Bug Fix

--- Demo_myKakuritsu_Linear.py
+++ Demo_myKakuritsu_Linear.py
@@ -46,8 +46,8 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(1, 32, 3, 1)
         self.conv2 = nn.Conv2d(32, 64, 3, 1)
-        self.fc1 = myKakuritsu_Linear_Obj(9216, 128)
-        self.fc2 = myKakuritsu_Linear_Obj(128, 10)
+        self.fc1 = myKakuritsu_Linear_Obj(9216, 128, 0.5)
+        self.fc2 = myKakuritsu_Linear_Obj(128, 10, 0.5)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -56,15 +56,17 @@ class Net(nn.Module):
         x = F.relu(x)
         x = F.max_pool2d(x, 2)
         x = torch.flatten(x, 1)
-        x = self.fc1(x, 0.5)
+        x = self.fc1(x)
         x = F.relu(x)
-        x = self.fc2(x, 0.5)
+        x = self.fc2(x)
         output = F.log_softmax(x, dim=1)
         return output
 
 
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
+    model.fc1.p = 0.5
+    model.fc2.p = 0.5
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         optimizer.zero_grad()
@@ -80,6 +82,8 @@ def train(args, model, device, train_loader, optimizer, epoch):
 
 def test(model, device, test_loader):
     model.eval()
+    model.fc1.p = 1.0
+    model.fc2.p = 1.0
     test_loss = 0
     correct = 0
     with torch.no_grad():
